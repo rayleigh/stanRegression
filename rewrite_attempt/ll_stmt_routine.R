9d804f0e5a7b7b176f3ea62e39fef5b32c42c9f1
Rayleigh L <rayleighl@yahoo.com>
2015-07-17 17:22:41 -0400

    Fixed bugs

--- rewrite_attempt/ll_stmt_routine.R
+++ rewrite_attempt/ll_stmt_routine.R
@@ -17,21 +17,22 @@ add_varying_term_rand_eff_term_to_ll_stmt <- function(parsed_term, ll_stmt)
   return(paste(ll_stmt, model_segment, sep = " + "))
 }
 
-add_varying_term_with_intercept_rand_eff_term_to_ll_stmt <- function(parsed_term, model_section)
+add_varying_term_with_intercept_rand_eff_term_to_ll_stmt <- function(parsed_term, ll_stmt)
 {
   model_segment <- paste(parsed_term$"trans_param_terms"[[1]], "[", parsed_term$"data_terms"[[1]], "[i], ", sep = "")
   
   model_segment_intercept <- paste(model_segment,  "1]", sep = "")
   
   model_segment_var <- paste(model_segment, "2]", sep = "")
-  model_segment_var <- paste(model_segment_var, parsed_term$"term_components"[[1]], sep = " * ")
+  model_segment_var <- paste(model_segment_var, parsed_term$"component_terms"[[1]], sep = " * ")
+  model_segment_var <- paste(model_segment_var, "[i]", sep = "")
   
   return(paste(ll_stmt, model_segment_intercept, model_segment_var, sep = " + "))
 }
 
 add_intercept_to_ll_stmt <- function(ll_stmt)
 {
-  return(paste("Intercept + ", ll_stmt, sep = ""))
+  return(paste("Intercept", ll_stmt, sep = ""))
 }
 
 add_ll_stmt_to_section_for_gaussian <- function(ll_stmt, resp_term_size, section)
@@ -48,7 +49,7 @@ add_ll_stmt_to_section <- function(ll_stmt, resp_term_size, ll_prob_term, sectio
 {
   section <- paste("vector[", resp_term_size, "] ", ll_prob_term, ";", section, sep = "") 
   section <- paste(section, "for (i in 1:", resp_term_size, ") {\n  ", sep = "")
-  section <- paste(section, ll_prob_term, "[i] <-", ll_stmt, ";}\n", sep = "")
+  section <- paste(section, ll_prob_term, "[i] <- ", ll_stmt, ";}\n", sep = "")
   
   return(section)  
 }
