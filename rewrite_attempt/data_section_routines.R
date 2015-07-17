06e211a944ac87e74b84ae559d3df0291f8976ca
Rayleigh L <rayleighl@yahoo.com>
2015-07-17 16:30:08 -0400

    Fixed bugs for how certain types were written

--- rewrite_attempt/data_section_routines.R
+++ rewrite_attempt/data_section_routines.R
@@ -1,12 +1,12 @@
 add_fixed_eff_term_to_data_section <- function(parsed_fixed_term, data_section)
 {
-  return(paste(data_section, create_var_data_line_for_array(parsed_fixed_term$"data_terms"[[1]], parsed_fixed_term$"size"[[1]], parsed_fixed_term$"stan_type"[[1]]), sep = ""))
+  return(paste(data_section, create_array_data_line(parsed_fixed_term$"data_terms"[[1]], parsed_fixed_term$"stan_data_type"[[1]], parsed_fixed_term$"size"[[1]]), sep = ""))
 }
 
 add_rand_eff_term_to_data_section <- function(parsed_term, data_section)
 {
   data_section <- paste(data_section, create_array_data_line(parsed_term$"data_terms"[[1]], "int", parsed_term$"size"[[1]]), sep = "")
-  return(paste(data_section, create_constant_data_line(parsed_term$"data_terms"[[2]], "int", "<lower = 1>"), sep = ""))
+  return(paste(data_section, create_constant_data_line(parsed_term$"data_terms"[[2]], "int", "<lower=1>"), sep = ""))
 }
 
 add_resp_term_to_data_section_for_gaussian <- function(parsed_resp_term, data_section)
@@ -14,19 +14,19 @@ add_resp_term_to_data_section_for_gaussian <- function(parsed_resp_term, data_se
   resp_var_name <- parsed_resp_term$"data_terms"[[1]]
   resp_var_size <- parsed_resp_term$"size"[[1]]
   
-  data_section <- paste(data_section, create_var_data_line_for_constant(resp_var_size, "int", "lower=1"), sep = "")
-  data_section <- paste(data_section, create_array_data_line(resp_var_name, resp_var_size, "real"), sep = "")
+  data_section <- paste(data_section, create_constant_data_line(resp_var_size, "int", "<lower=1>"), sep = "")
+  data_section <- paste(data_section, create_array_data_line(resp_var_name, "real", resp_var_size), sep = "")
 
   return(data_section)
 }
 
-add_resp_term_to_data_section_for_binomial <- function(parsed_resp_term, data_var_list)
+add_resp_term_to_data_section_for_binomial <- function(parsed_resp_term, data_section)
 {
   resp_var_name <- parsed_resp_term$"data_terms"[[1]]
   resp_var_sample_size_name <- parsed_resp_term$"data_terms"[[parsed_resp_term$"special_vectors_index"$"sample_size"]]
   resp_var_size <- parsed_resp_term$"size"[[1]]
   
-  data_section <- paste(data_section, create_var_data_line_for_constant(resp_var_size, "int", "lower=1"), sep = "")
+  data_section <- paste(data_section, create_constant_data_line(resp_var_size, "int", "<lower=1>"), sep = "")
   data_section <- paste(data_section, create_array_data_line(resp_var_name, "int", resp_var_size), sep = "")
   data_section <- paste(data_section, create_array_data_line(resp_var_sample_size_name, "int", resp_var_size), sep = "")
   
