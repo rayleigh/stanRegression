3f652b1cf63ce3a856accf7c50fa5f9b93d61e92
Rayleigh L <rayleighl@yahoo.com>
2015-07-17 16:38:38 -0400

    Fixed some bugs

--- rewrite_attempt/param_section_routine.R
+++ rewrite_attempt/param_section_routine.R
@@ -1,6 +1,6 @@
 add_fixed_eff_term_to_param_section <- function(parsed_term, param_section)
 {
-  return(paste(param_section, create_param_constant_line(parsed_term$"param_terms"[[1]])))
+  return(paste(param_section, create_param_constant_line(parsed_term$"param_terms"[[1]]), sep = ""))
 }
 
 add_varying_term_rand_eff_term_to_param_section <- function(parsed_term, param_section)
@@ -25,20 +25,20 @@ add_intercept_to_param_section <- function(param_section)
 
 add_sigma_to_param_section <- function(param_section)
 {
-  return(paste(param_section, create_param_constant_line("sigma", "lower=0")))
+  return(paste(param_section, create_param_constant_line("sigma", "<lower=0>"), sep = ""))
 }
 
 create_param_constant_line <- function(var_name, var_constr = "", var_type = "real")
 {
-  return(paste(var_type, var_constr, " ", var_name, ";"), sep = "")
+  return(paste(var_type, var_constr, " ", var_name, ";", sep = ""))
 }
 
 create_param_vector_line <- function(var_name, array_size, var_constr = "", var_type = "vector")
 {
-  return(paste(var_type, var_constr, "[", array_size,"]", var_name, ";"), sep = "")
+  return(paste(var_type, var_constr, "[", array_size,"] ", var_name, ";", sep = ""))
 }
 
 create_param_matrix_line <- function(var_name, num_row, num_col, var_constr = "", var_type = "matrix")
 {
-  return(paste(var_type, var_constr, "[", num_row, ",", num_col , "] ", var_name, ";"), sep = "")
+  return(paste(var_type, var_constr, "[", num_row, ", ", num_col , "] ", var_name, ";", sep = ""))
 }
