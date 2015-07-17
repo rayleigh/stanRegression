a1f26b76ab02bbbac99f80750a2f7268304eca26
Rayleigh L <rayleighl@yahoo.com>
2015-07-17 17:29:22 -0400

    Fixed bugs

--- rewrite_attempt/generated_section_routine.R
+++ rewrite_attempt/generated_section_routine.R
@@ -1,16 +1,16 @@
 create_generated_section_for_gaussian <- function(ll_stmt, resp_term)
 {
-  gen_section <- 'real dev;\ndev <- 0;\n'
-  gen_section <- add_ll_stmt_to_section_for_gaussian(ll_stmt, resp_term$"size"[[1]], generated_section)
-  gen_section <- paste("dev <- dev + (-2) * normal_log(", resp_term$"data_terms"[[1]], ", mu, sigma);\n", sep = "")
+  gen_section <- 'real dev;dev <- 0;'
+  gen_section <- add_ll_stmt_to_section_for_gaussian(ll_stmt, resp_term$"size"[[1]], gen_section)
+  gen_section <- paste(gen_section, "dev <- dev + (-2) * normal_log(", resp_term$"data_terms"[[1]], ", mu, sigma);\n", sep = "")
   return(gen_section)
 }
 
 create_generated_section_for_binomial <- function(ll_stmt, resp_term)
 {
   gen_section <- 'real dev;\ndev <- 0;\n'
-  gen_section <- add_ll_stmt_to_section_for_binomial(ll_stmt, resp_term$"size"[[1]], generated_section)
+  gen_section <- add_ll_stmt_to_section_for_binomial(ll_stmt, resp_term$"size"[[1]], gen_section)
   temp_params_section <- paste(resp_term$"data_terms"[[1]], resp_term$"data_terms"[[resp_term$"special_vectors_index"$"sample_size"]], "p", sep = ", ")
-  gen_section <- paste("dev <- dev + (-2) * binomial_logit_log(", temp_params_section, ");\n", sep = "")
+  gen_section <- paste(gen_section, "dev <- dev + (-2) * binomial_logit_log(", temp_params_section, ");\n", sep = "")
   return(gen_section)
 }
